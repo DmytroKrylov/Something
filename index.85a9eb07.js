1bbd4d7d25eb557964a8a283ac34eab64935744e
DmytroKrylov <120196125+DmytroKrylov@users.noreply.github.com>
2024-02-05 17:59:59 +0000

    Deploying to gh-pages from @ DmytroKrylov/Something@1f1d9730dbd670548a7b6322df80951974989bd2 🚀

---
+++ index.85a9eb07.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".ratingjs");document.querySelectorAll(".ratingsjs").forEach((function(n){n.addEventListener("click",(function(){t.textContent=n.textContent}))}))}();
+//# sourceMappingURL=index.85a9eb07.js.map
---
+++ index.dc6eec5b.js
@@ -0,0 +1,2 @@
+let t=document.querySelector(".ratingjs");document.querySelectorAll(".ratingsjs").forEach((e=>{e.addEventListener("click",(()=>{t.textContent=e.textContent}))}));
+//# sourceMappingURL=index.dc6eec5b.js.map
