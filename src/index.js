af1cee08f613a87e49eac97c27327937fc5c05a6
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-02-05 13:42:00 +0200

    98%

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-// const main = document.querySelector('.main');
-// const themeswitcher = document.querySelector('.themeswitcher');
-// const footer = document.querySelector('.footer');
-// const desc = document.querySelector('.desc');
-// const headerJs = document.querySelector('.headerjs');
-// const logojs = document.querySelector('.logojs')
-// const titlesecondjs = document.querySelector('.titlesecondjs')
-// const cardjs = document.querySelector('.cardjs')
-// const logoftjs = document.querySelector('.logoftjs')
+const themeswitcher = document.querySelector('.themeswitcher');
+const darkalljs = document.querySelectorAll('.darkalljs');
+const whitealljs = document.querySelectorAll('.whitealljs')
+console.log(whitealljs)
 
-// themeswitcher.addEventListener('click', () => themeswitcher.classList.toggle('active'));
-// footer.addEventListener('click', () => footer.classList.toggle('active'));
-// desc.addEventListener('click', () => desc.classList.toggle('active'));
-// headerJs.addEventListener('click', () => headerJs.classList.toggle('active'));
-// logojs.addEventListener('click', () => logojs.classList.toggle('active'));
-// titlesecondjs.addEventListener('click', () => titlesecondjs.classList.toggle('active'));
-// cardjs.addEventListener('click', () => cardjs.classList.toggle('active'));
-// logoftjs.addEventListener('click', () => logoftjs.classList.toggle('active'));
+themeswitcher.addEventListener('click', () => themeswitcher.classList.toggle('active'));
 
-// themeswitcher.addEventListener('click', () => {
-//     main.classList.toggle('dark')
-//     footer.classList.toggle('dark')
-//     desc.classList.toggle('white')
-//     headerJs.classList.toggle('dark')
-//     logojs.classList.toggle('white')
-//     titlesecondjs.classList.toggle('white')
-//     cardjs.classList.toggle('dark')
-//     logoftjs.classList.toggle('white')
-// })
+
+darkalljs.forEach((darkjs) => {
+  darkjs.addEventListener('click', () => darkjs.classList.toggle('active'));
+
+  themeswitcher.addEventListener('click', () => {
+    darkjs.classList.toggle('dark')
+  })
+});
+
+
+whitealljs.forEach((whitejs) => {
+    whitejs.addEventListener('click', () => whitejs.classList.toggle('active'));
+
+  themeswitcher.addEventListener('click', () => {
+    whitejs.classList.toggle('white')
+  })
+});
+
